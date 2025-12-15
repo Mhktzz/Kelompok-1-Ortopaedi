8d3aa5c07acc68664cdfa3af0ef59f3f947001d8
Zaki Farhan Rifai <zakifarhanrifai@gmail.com>
2025-12-15 22:56:40 +0700

    add migrate,model,login

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,3 @@ use App\Http\Controllers\AuthController;
 // LOGIN
 Route::get('/', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/', [AuthController::class, 'login'])->name('login.process');
-
-// REGISTER
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register'])->name('register.process');
